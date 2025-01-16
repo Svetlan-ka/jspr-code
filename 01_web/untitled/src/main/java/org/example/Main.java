e731ccf90a507bde89fe052da533f436d58c58de
Svetlan-ka <sv.chapova@yandex.ru>
2025-01-17 01:45:38 +0700

    CRUD

---
+++ 01_web/untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Main {
+
+
+    private final AtomicLong idPost = new AtomicLong();
+    private final Map<Long, Post> storagePost = new ConcurrentHashMap<>();
+
+    public List<Post> all() {
+        return new ArrayList<>(storagePost.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(storagePost.get(id));
+    }
+
+    public Post save(Post post) {
+        var postId = post.getId();
+        if (postId == 0)
+            storagePost.put(idPost.incrementAndGet(), post);
+
+        if (postId != 0) {
+            if (storagePost.containsKey(postId))
+                storagePost.put(postId, post);
+        } else {
+            throw new RuntimeException();
+        }
+
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (storagePost.containsKey(id)) {
+            storagePost.remove(id);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+
+}
